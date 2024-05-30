4832ea946fe88c8a4901ec7274b9345644a30e5e
corinB <gkdisrha2020@gmail.com>
2024-05-30 20:03:07 +0900

    project start

---
+++ butler/src/main/java/com/example/butler/ButlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.butler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButlerApplication.class, args);
+	}
+
+}
---
+++ butler/src/test/java/com/example/butler/ButlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.butler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
